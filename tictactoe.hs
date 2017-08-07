3732e35f3703bddd3d965e885df99cd70c8ace6f
charlieshanley <charles.scott.hanley+git@gmail.com>
2017-08-06 23:28:47 -0400

    finished funs to make moves; documented

--- tictactoe.hs
+++ tictactoe.hs
@@ -1,11 +1,13 @@
 import Data.List
+import Data.Maybe
+import Data.Tree
 
 -- main = do
 --     putStr intro
 --     play newBoard
 
 
-
+-- Data types to represent our scenario
 data Mark = X | O | Empty deriving (Eq, Show, Read)
 
 data Space = Space { getMark :: Mark, getInd :: Int }
@@ -22,18 +24,21 @@ type Board = [[Space]]
 data Endgame = Draw | IWin | YouWin
 
 
+-- Constants to get us started
 intro = "\nI am TicTacToe9000. Welcome to my Tic-tac-Temple. Let's play.\n" ++
         "You go first. You're X; I'm O. Enter a number to mark a space.\n\n"
 
 newBoard :: Board
 newBoard = (map . map) (Space Empty) [[1..3], [4..6], [7..9]]
 
+
+-- Test for endgame
 endgame :: Board -> Maybe Endgame
-endgame board
-    | elem [O, O, O] . getWinLines $ board         = Just IWin
-    | elem [X, X, X] . getWinLines $ board         = Just YouWin
-    | notElem Empty . map getMark . toList $ board = Just Draw
-    | otherwise                                    = Nothing
+endgame b
+    | elem [O, O, O] . getWinLines $ b         = Just IWin
+    | elem [X, X, X] . getWinLines $ b         = Just YouWin
+    | notElem Empty . map getMark . concat $ b = Just Draw
+    | otherwise                                = Nothing
 
 getWinLines :: Board -> [[Mark]]
 getWinLines b = (map . map) getMark spaceLines
@@ -45,20 +50,39 @@ diagonal [[]] = []
 diagonal ((x:_):rs) = x : (diagonal $ map tail rs)
 
 
+-- Make moves on the board
 makeMark :: Mark -> Space -> Maybe Space
 makeMark new (Space Empty n) = Just (Space new n)
 makeMark _ (Space X _) = Nothing
 makeMark _ (Space O _) = Nothing
 
-move :: Board -> Player -> Int -> Maybe Board
-move b Human i = 
-move b Computer i = 
+maybeChangeElement :: (a -> Maybe a) -> [[a]] -> Int -> Int -> Maybe [[a]]
+maybeChangeElement f orig r c = (beforeRs ++) . (++ afterRs) . (:[]) <$> newR
     where
-        inds = (map . map) getInd newBoard
+        (beforeRs, (oldR:afterRs)) = splitAt r orig
+        (beforeVs, (oldV:afterVs)) = splitAt c oldR
+        newR = (beforeVs ++) . (++ afterVs) . (:[]) <$> newV
+        newV = f oldV
+
+
+markSpace :: Mark -> Maybe Int -> Maybe Int -> Board -> Maybe Board
+markSpace m row col b = do
+    r <- row
+    c <- col
+    maybeChangeElement (makeMark m) b r c
+
+
+move :: Board -> Mark -> Int -> Maybe Board
+move b m i = markSpace m row col b
+    where
+        inds = (map . map) getInd b
         row = findIndex (elem i) inds
-        col =  row >>= (\r -> findIndex ( == 1) $ inds !! r)
+        col =  row >>= (findIndex (== 1) . (!!) inds)
 
 
+
+-- Decide which move to make
+emptyInds :: Board -> [Int]
 
 
 -- play :: Board -> Board
