97dca023d29a27baa7f63227ee5499d75680321b
charlieshanley <charles.scott.hanley+git@gmail.com>
2017-08-27 17:28:51 -0400

    tree look ahead working but too slow

--- Tictactoe.hs
+++ Tictactoe.hs
@@ -1,6 +1,7 @@
 module Tictactoe
     (
     Mark(..),
+    Space(..),
     Board,
     Endgame,
     newBoard,
@@ -15,12 +16,26 @@ import GHC.Exts (groupWith)
 
 
 data Mark = X | O | Empty deriving (Eq, Show, Read)
+instance Enum Mark where
+    succ Empty = X
+    succ X     = O
+    succ O     = X
+    toEnum 0        = Empty
+    toEnum x
+        | odd x     = X
+        | otherwise = O
+    fromEnum Empty = 0
+    fromEnum X     = 1
+    fromEnum O     = 2
+
 data Space = Space { getMark :: Mark, getInd :: Int }
 instance Show Space where
     show (Space X _)     = "X"
     show (Space O _)     = "O"
     show (Space Empty n) = show n
+
 type Board = [[Space]]
+
 data Endgame = Draw | OWins | XWins deriving (Eq, Show, Read)
 
 
@@ -32,10 +47,12 @@ newBoard = (map . map) (Space Empty) [[1..3], [4..6], [7..9]]
 
 endgame :: Board -> Maybe Endgame
 endgame b
-    | elem [O, O, O] . getWinLines $ b         = Just OWins
-    | elem [X, X, X] . getWinLines $ b         = Just XWins
-    | notElem Empty . map getMark . concat $ b = Just Draw
-    | otherwise                                = Nothing
+    | elem [O, O, O] winLines = Just OWins
+    | elem [X, X, X] winLines = Just XWins
+    | notElem Empty  marks    = Just Draw
+    | otherwise                 = Nothing
+    where winLines = getWinLines b
+          marks = map getMark . concat $ b
 
 getWinLines :: Board -> [[Mark]]
 getWinLines b = (map . map) getMark spaceLines
--- TictactoeSolver.hs
+++ TictactoeSolver.hs
@@ -1,5 +1,28 @@
 import Tictactoe
-import Data.Tree
+
+
+data Rose a = Rose a [Rose a] deriving (Show, Eq)
+
+lookAhead :: Mark -> Board -> Rose Board
+lookAhead m b = Rose b (nextTurn `map` allMoves m b)
+    where nextTurn = lookAhead (succ m)
+
+
+allMoves :: Mark -> Board -> [Board]
+allMoves m b = moveI `map` (emptyInds b)
+    where moveI = \i -> unsafeMove m i b
+
+emptyInds :: Board -> [Int]
+emptyInds = map getInd . filter ((==Empty) . getMark) . concat
+
+
+
+
+-- showBoard :: Board -> String
+-- showBoard = unlines . map unwords . (map . map) show
+
+-- -- play :: Board -> Board
+
 
 main = undefined
 
@@ -7,26 +30,3 @@ main = undefined
 
 intro = "\nI am TicTacToe9000. Welcome to my Tic-tac-Temple. Let's play.\n" ++
         "You go first. You're X; I'm O. Enter a number to mark a space.\n\n"
-
-
-lookAhead :: Board -> Tree Board
-lookAhead brd = Tree brd (map lookAhead $ allMoves )
-
-
-allMoves :: Mark -> Board -> [Board]
-allMoves m b = moveI `map` legalMoves
-    where legalMoves = emptyInds b
-          moveI = \i -> unsafeMove m i b
-
-emptyInds :: Board -> [Int]
-emptyInds = map getInd . filter ((==Empty) . getMark) . concat
-
-
-
--- type GameState = Tree (Int, Board)
-
-
--- showBoard :: Board -> String
--- showBoard = unlines . map unwords . (map . map) show
-
--- -- play :: Board -> Board
