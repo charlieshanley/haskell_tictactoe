2737233d43cf4fbf7625b99ae4ade59b9052ace1
charlieshanley <charles.scott.hanley+git@gmail.com>
2017-08-31 22:04:50 -0400

    hastyMaximin. Reorganized.

--- TictactoeSolver.hs
+++ TictactoeSolver.hs
@@ -13,7 +13,7 @@ data Rose a = Rose a [Rose a] deriving (Show, Eq)
 instance Functor Rose where
     f `fmap` (Rose x ys) = Rose (f x) $ (map . fmap) f ys
 
-data Assessment = Assessment {gameState :: GameState, merit :: Int}
+data Assessment = Assessment {gameState :: GameState, merit :: Float}
 
 -- The main thing
 decide :: (Rose GameState -> Rose Assessment) -> GameState -> Maybe Int
@@ -37,18 +37,10 @@ allMoves gs@(GameState _ m b) = moveThere `map` legalMoves
 emptyInds :: Board -> [Int]
 emptyInds = map getInd . filter ((==Empty) . getMark) . concat
 
-
 -------------------------------------------------------------------------------
--- Score board states and select a good move
+-- Evaluate the tree of GameStates
 
--- pickMove = undefined
-pickMove :: Rose Assessment -> Maybe Int
-pickMove (Rose _ []) = Nothing
-pickMove (Rose current potential) = lastMove <$> chosen
-    where candidates = map unrose potential
-          bestMerit = maximum . map merit $ candidates
-          chosen = gameState <$> find ((bestMerit==) . merit) candidates
-
+-- Assumes perfect oponent; does not care how long game takes.
 maximin :: Rose GameState -> Rose Assessment
 maximin (Rose gs []) = Rose (Assessment gs (scoreGS gs)) []
 maximin (Rose gs rs) = Rose (Assessment gs (review children)) children
@@ -56,16 +48,39 @@ maximin (Rose gs rs) = Rose (Assessment gs (review children)) children
           review = superlative . map (merit . unrose)
           superlative = if whoseTurn gs == O then maximum else minimum
 
-scoreGS :: GameState -> Int
+-- Assumes perfect oponent; prefers to win quickly and lose slowly.
+hastyMaximin :: Rose GameState -> Rose Assessment
+hastyMaximin (Rose gs []) = Rose (Assessment gs (scoreGS gs)) []
+hastyMaximin (Rose gs rs) = Rose (Assessment gs (review children)) children
+    where children = map maximin rs
+          review = (0.5 *) . superlative . map (merit . unrose)
+          superlative = if whoseTurn gs == O then maximum else minimum
+
+
+scoreGS :: (Num a) => GameState -> a
 scoreGS = score . endgame . board
 
-score :: Maybe Endgame -> Int
+score :: (Num a) => Maybe Endgame -> a
 score (Just XWins) = negate 1
 score (Just OWins) = 1
 score _            = 0
 
 unrose :: Rose a -> a
 unrose (Rose x _) = x
+
+
+-------------------------------------------------------------------------------
+-- Pick the best move
+
+pickMove :: Rose Assessment -> Maybe Int
+pickMove (Rose _ []) = Nothing
+pickMove (Rose current potential) = lastMove <$> chosen
+    where candidates = map unrose potential
+          bestMerit = maximum . map merit $ candidates
+          chosen = gameState <$> find ((bestMerit==) . merit) candidates
+
+-------------------------------------------------------------------------------
+-- development utilities
 
 showTree :: (Int -> a -> String) -> Rose a -> String
 showTree showFun r = show' 0 r
@@ -77,9 +92,6 @@ showInt indent = (replicate indent ' ' ++) . show
 showGame :: Int -> GameState -> String
 showGame i = unlines . map (indent . unwords) . (map . map) show . board
     where indent s = replicate i ' ' ++ s
-
--------------------------------------------------------------------------------
--- testing
 
 draw = (return newGame) >>= move 1 >>= move 5 >>= move 3 >>= move 2 >>= move 8
 iwin = (return newGame) >>= move 1 >>= move 7 >>= move 4 >>= move 9 >>= move 2
