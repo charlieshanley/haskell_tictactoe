85577aec87d182e876e02d8a70250423dfb6131a
charlieshanley <charles.scott.hanley+git@gmail.com>
2017-09-04 15:38:29 -0400

    Fixed double entry bug by moving from getChar to getLine. This project is complete for now.

--- PlayTictactoe.hs
+++ PlayTictactoe.hs
@@ -2,6 +2,7 @@ import Tictactoe
 import TictactoeSolver
 import Data.Maybe (fromJust)
 import Data.Char (isNumber)
+import Control.Monad (when)
 
 main :: IO ()
 main = putStrLn intro >>
@@ -9,33 +10,33 @@ main = putStrLn intro >>
 
 playAI :: GameState -> IO ()
 playAI gs =
+    (putStrLn . showBoard . board) gs >>
     case endgame . board $ gs of
         Just end -> putStrLn . announce $ end
         Nothing -> turn gs >>= playAI
-    where turn = if whoseTurn gs == X then playerTurn else return . aiTurn
+    where turn = if whoseTurn gs == X then playerTurn else aiTurn
 
-
-playPVP :: GameState -> IO ()
-playPVP = undefined
-
-
-aiTurn :: GameState -> GameState
-aiTurn gs = flip unsafeMove gs . fromJust . decide hastyMaximin $ gs
+aiTurn :: GameState -> IO GameState
+aiTurn gs = 
+    putStrLn "My move:" >>
+    return (flip unsafeMove gs . fromJust . decide hastyMaximin $ gs)
 
 
 playerTurn ::  GameState -> IO GameState
 playerTurn gs0 =
-    (putStrLn . showBoard . board) gs0  >>
-    getChar                      >>= \c ->
-    maybe (tryAgain gs0) return (maybeInt c >>= flip move gs0)
+    putStrLn "Your move:" >>
+    getLine        >>= \l ->
+    maybe (tryAgain l gs0) return (maybeInt l >>= flip move gs0)
 
-tryAgain :: GameState -> IO GameState
-tryAgain gs = putStrLn "That is not a valid move. Pick again." >> playerTurn gs
+tryAgain :: String -> GameState -> IO GameState
+tryAgain c gs =
+    putStrLn (c ++ " is not a valid move. Pick again.") >>
+    playerTurn gs
 
-maybeInt :: Char -> Maybe Int
-maybeInt c
+maybeInt :: String -> Maybe Int
+maybeInt (c:[])
     | isNumber c = Just (read [c] :: Int)
-    | otherwise  = Nothing
+maybeInt _       = Nothing
 
 announce :: Endgame -> String
 announce Draw  = "It's a draw!"
